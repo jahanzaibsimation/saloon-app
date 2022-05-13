25286c21fa777d3362cb61a757d49ed895f1b514
jahanzaibsimation <jahanzaib.simationstudio@gmail.com>
2022-05-13 15:03:59 -0400

    Login and Signup Form

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider } from 'native-base';
+import { StyleSheet, Text, View } from 'react-native';
+import { Login } from './components/screens/Login';
+import React from 'react'
+import SignUp from './components/screens/SignUp';
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        {/* <Login /> */}
+        <SignUp />
+        <StatusBar style="auto" />
+      </View>
+    </NativeBaseProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FBFCFF',
+
+  },
+});
