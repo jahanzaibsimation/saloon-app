08ebedf2d77408d967c90bf5cc1d5f68427984cd
Jahanzaib Ali Khan <engr.jahanzaibalikhan@gmail.com>
2022-05-22 21:39:40 +0500

    Drawer Data change

---
+++ dummyData/drawer2.js
@@ -0,0 +1,58 @@
+export const drawerData = [
+    {
+        id: 1,
+        name: 'Annoucements',
+        icon: {
+            name: 'bell',
+            service: 'FontAwesome5',
+        },
+    },
+    {
+        id: 2,
+        name: 'Performance',
+        icon: {
+            name: 'piechart',
+            service: 'AntDesign',
+        }
+    },
+    {
+        id: 3,
+        name: 'Engagement',
+        icon: {
+            name: 'like1',
+            service: 'AntDesign',
+        },
+    },
+    {
+        id: 4,
+        name: 'Development',
+        icon: {
+            name: 'graduation-cap',
+            service: 'FontAwesome',
+        },
+    },
+    {
+        id: 5,
+        name: 'Onboarding',
+        icon: {
+            name: 'text-box-check',
+            service: 'MaterialCommunityIcons',
+        },
+    },
+    {
+        id: 6,
+        name: 'Employees',
+        icon: {
+            name: 'user-cog',
+            service: 'FontAwesome5',
+        },
+    },
+    {
+        id: 7,
+        name: 'Resources',
+        icon: {
+            name: 'paperclip',
+            service: 'FontAwesome5',
+        },
+    }
+]
