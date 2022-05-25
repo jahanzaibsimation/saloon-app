88e54310713b92e742aafb9c7c035eaf2180b8a2
jahanzaibsimation <jahanzaib.simationstudio@gmail.com>
2022-05-25 18:59:06 -0400

    Install Signature Pad

--- src/components/PdfViewer.js
+++ src/components/PdfViewer.js
@@ -1,13 +1,13 @@
 import { View, Text, } from 'react-native'
 import React from 'react'
-import { WebView } from 'react-native-webview'
+// import { WebView } from 'react-native-webview'
 import Constants from 'expo-constants'
 
 const PdfViewer = () => {
     return (
         <View style={{ flex: 1, marginTop: Constants.statusBarHeight }}>
             <Text>PdfViewer</Text>
-            <WebView source={{ uri: 'https://enos.itcollege.ee/~jpoial/allalaadimised/reading/Advanced-java.pdf' }} />
+            {/* <WebView source={{ uri: 'https://enos.itcollege.ee/~jpoial/allalaadimised/reading/Advanced-java.pdf' }} /> */}
         </View>
     )
 }
